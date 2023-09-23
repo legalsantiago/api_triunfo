95db4c68f49e3bdf527598ec5f4bcffabe9bc7ad
santi-mb <102435538+legalsantiago@users.noreply.github.com>
2023-09-23 12:26:41 -0400

    Update base.py debug to false

--- drf/settings/base.py
+++ drf/settings/base.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dt9uf)k5bl(rd^3dm$fu2s2=+bnez$5e@ggzlm=%7g&hz7t&ks'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
