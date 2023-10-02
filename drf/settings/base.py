351baec96f621c99df8bc9390c6fdd959dfc42e5
legalsantiago <smora430@gmail.com>
2023-10-02 09:49:32 -0400

    changes config ebextension-settings-static-root

--- drf/settings/base.py
+++ drf/settings/base.py
@@ -135,9 +135,9 @@ AUTH_USER_MODEL = 'users.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+import os
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
